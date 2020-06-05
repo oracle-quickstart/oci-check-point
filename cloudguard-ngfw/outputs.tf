d260e916542f89cd636770a52b6c0d6e99bf6795
Lucas Gomes <lucas.gomes@oracle.com>
2020-06-05 00:44:44 -0700

    Cleanup output and added instruction
    I removed all non-used outputs and added an output with initial instruction that should be visible from Terraform cli only

--- cloudguard-ngfw/outputs.tf
+++ cloudguard-ngfw/outputs.tf
@@ -2,22 +2,6 @@ output "subscription" {
   value = data.oci_core_app_catalog_subscriptions.mp_image_subscription.*.app_catalog_subscriptions
 }
 
-output "vcn_id" {
-  value = ! local.use_existing_network ? join("", oci_core_vcn.vcn.*.id) : var.vcn_id
-}
-
-output "subnet_id" {
-  value = ! local.use_existing_network ? join("", oci_core_subnet.public_subnet.*.id) : var.subnet_id
-}
-
-output "vcn_cidr_block" {
-  value = ! local.use_existing_network ? join("", oci_core_vcn.vcn.*.cidr_block) : var.vcn_cidr_block
-}
-
-output "nsg_id" {
-  value = join("", oci_core_network_security_group.nsg.*.id)
-}
-
 output "instance_public_ip" {
   value = oci_core_instance.simple-vm.public_ip
 }
@@ -30,3 +14,25 @@ output "instance_https_url" {
   value = "https://${oci_core_instance.simple-vm.public_ip}"
 }
 
+output "initial_instruction" {
+value = <<EOT
+
+1.  Open an SSH client.
+2.  Use the following information to connect to the instance
+username: admin
+IP_Address: ${oci_core_instance.simple-vm.public_ip}
+SSH Key
+For example:
+
+$ ssh –i id_rsa admin@${oci_core_instance.simple-vm.public_ip}
+
+3.  Set the user password for the administrator. Enter the command: set user admin password
+4. Save the configuration. Enter the command: save config
+
+After saving the password, you should run the first time wizard in the Gaia Portal:
+
+1.  In a web browser, connect to the Gaia Portal: https://${oci_core_instance.simple-vm.public_ip}
+2.  Follow the First Time Configuration Wizard.
+3.  For additional details follow the documentation.
+EOT
+}
